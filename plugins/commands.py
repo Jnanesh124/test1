b7652932045bec13867c024c96279f6bde6f9105
Matrix TG <108185896+matrixbotz0@users.noreply.github.com>
2023-03-11 16:26:43 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,6 +1,6 @@
 import logging
 logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+                    format='(asctime) - (name) - (levelname) - (message)')
 logger = logging.getLogger(__name__)
 
 import pyrogram
