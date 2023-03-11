b3b7a920cf4c0b23311cbd49613e15bf1b413795
Matrix TG <108185896+matrixbotz0@users.noreply.github.com>
2023-03-11 20:41:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import pyrogram
 
 import logging
 logging.basicConfig(level=logging.DEBUG,
-                    format='(asctime)s - (name)s - (levelname)s - (message)s')
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
 import os
