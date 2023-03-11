d26dfa60ca3d8a57f8f10263d56f704764879677
Matrix TG <108185896+matrixbotz0@users.noreply.github.com>
2023-03-11 20:43:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "6008504043:AAHtEwk0fbPZF2dXfYkuXh5y6PPt-9jlwXU")
-    API_ID = int(os.environ.get("API_ID", "11615722")
+    API_ID = int(os.environ.get("API_ID", "11615722"))
     API_HASH = os.environ.get("API_HASH", "c992746520e8886d3330de2ec9a1a3a7")
     DB_URL = os.environ.get("DATABASE_URL", "mongodb+srv://auto:auto@cluster0.xikol9h.mongodb.net/?retryWrites=true&w=majority")
