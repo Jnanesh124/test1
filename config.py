2816494a05a506c06ae59e936f1900993fe2c5be
Jnanesh <99884442+Jnanesh124@users.noreply.github.com>
2024-01-11 00:14:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6008504043:AAHtEwk0fbPZF2dXfYkuXh5y6PPt-9jlwXU")
-    API_ID = int(os.environ.get("API_ID", "11615722"))
-    API_HASH = os.environ.get("API_HASH", "c992746520e8886d3330de2ec9a1a3a7")
-    DB_URL = os.environ.get("DATABASE_URL", "mysql://dt_admin:dt2016@localhost:3308/dreamteam_db")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6885232721:AAGkYm_7SW1IUdwJYTJ8eZwC4PX39rU3_uk")
+    API_ID = int(os.environ.get("API_ID", "22231458"))
+    API_HASH = os.environ.get("API_HASH", "ad92f01e4a90cddebbea0ad16fa23026")
+    DB_URL = os.environ.get("DATABASE_URL", "mongodb+srv://jnanesh:jnanesh@cluster0.8pzxa6s.mongodb.net/?retryWrites=true&w=majority")
