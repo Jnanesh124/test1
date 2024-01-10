4d34754e098b15d916ade9e42359656d70f0e833
Jnanesh <99884442+Jnanesh124@users.noreply.github.com>
2024-01-11 00:18:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "6885232721:AAGkYm_7SW1IUdwJYTJ8eZwC4PX39rU3_uk")
-    API_ID = int(os.environ.get("API_ID", "22231458"))
-    API_HASH = os.environ.get("API_HASH", "ad92f01e4a90cddebbea0ad16fa23026")
+    API_ID = int(os.environ.get("API_ID", "20846870"))
+    API_HASH = os.environ.get("API_HASH", "a864594cb1752ff03948066e529fcf0a")
     DB_URL = os.environ.get("DATABASE_URL", "mongodb+srv://jnanesh:jnanesh@cluster0.8pzxa6s.mongodb.net/?retryWrites=true&w=majority")
